2491efde9f086eade562c9ce3054a68164ddc02b
Dave Briccetti <daveb@davebsoft.com>
2019-07-25 11:07:10 -0700

    Highlight the selected egg

--- sketch.js
+++ sketch.js
@@ -1,21 +1,25 @@
 let jumpStartTimeMs = null;  // When the jump started or null if not jumping
 
 let positions;
+let player_index = 2;
 
 function setup() {
   createCanvas(800, 800, WEBGL);
-  const p1 = createVector(   0, 0, 0);
-  const p2 = createVector(-300, 0, 0);
+  const p1 = createVector(-300, 0, 0);
+  const p2 = createVector(   0, 0, 0);
   const p3 = createVector( 300, 0, 0);
   positions = [p1, p2, p3];
 }
 
 function draw() {
   background(220);
-  const p1 = positions[0];
+  const p1 = positions[player_index];
 
   if (keyIsPressed) {
     switch (key) {
+      case '1': player_index = 0; break;
+      case '2': player_index = 1; break;
+      case '3': player_index = 2; break;
       case 'w': p1.z -= 5; break;
       case 's': p1.z += 5; break;
       case 'a': p1.x -= 5; break;
@@ -29,10 +33,10 @@ function draw() {
   }
 
   noStroke();
-  ambientMaterial('white');
   directionalLight(255, 255, 255, 1, 1, -1);
 
-  positions.forEach(p => {
+  positions.forEach((p, i) => {
+    ambientMaterial(i === player_index ? 'green' : 'white');
     push();
     translate(p.x, p.y, p.z);
     ellipsoid(40, 60);
