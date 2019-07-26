c1822c9fc28c4a9bff39049c6b90345db4c9d07c
Dave Briccetti <daveb@davebsoft.com>
2019-07-26 12:39:21 -0700

    Add rotation, a floor, faster movement with shift, simultaneous key presses

--- sketch.js
+++ sketch.js
@@ -1,23 +1,38 @@
+// p5.js Jump Example, AKA Jumping Eggs
+
 let jumpStartTimeMs = [];  // When each jump started or null if not jumping
 let positions = [];
-let player_index = 0;
+let selected_player = 0;
+let mouseHasMoved = false;
 
 function setup() {
   createCanvas(800, 700, WEBGL);
+  smooth();
   for (let x = -300; x <= 300; x += 100) {
     positions.push(createVector(x, 0, 0))
   }
 }
 
 function draw() {
-  background(220);
-  const pos = positions[player_index];
+  background('lightblue');
+  const pos = positions[selected_player];
   handleInput(pos);
+
+  if (mouseHasMoved && mouseY >= 0 && mouseY < height)
+    rotateY(cMap(mouseX, 0, width - 1, -TAU / 16, TAU / 16));
+
+  for (let x = -500; x <= 500; x += 50) {
+    push();
+    translate(x, 60, 0);
+    rotateX(TAU * 0.25);
+    plane(10, 2400);
+    pop();
+  }
   ambientMaterial('white');
   directionalLight(255, 255, 255, 1, 1, -1);
 
   positions.forEach((p, i) => {
-    if (i === player_index) stroke('white'); else noStroke();
+    if (i === selected_player) stroke('white'); else noStroke();
     push();
     translate(p.x, p.y - getJumpHeight(i), p.z);
     ellipsoid(40, 60);
@@ -25,22 +40,25 @@ function draw() {
   });
 }
 
+function mouseMoved() {
+  mouseHasMoved = true;
+}
+
 function handleInput(pos) {
-  if (keyIsPressed) {
-    const num = Number(key);
-    if (num >= 1 && num <= positions.length) {
-      player_index = num - 1
-    } else {
-      switch (key) {
-        case 'w': pos.z -= 5; break;
-        case 's': pos.z += 5; break;
-        case 'a': pos.x -= 5; break;
-        case 'd': pos.x += 5; break;
-        case ' ':
-          if (!jumpStartTimeMs[player_index]) {
-            jumpStartTimeMs[player_index] = millis();
-          }
-          break;
+  if (!keyIsPressed) return;
+
+  const num = Number(key);
+  if (num >= 1 && num <= positions.length) {
+    selected_player = num - 1
+  } else {
+    const shiftMult = keyIsDown(SHIFT) ? 10 : 1;
+    if (keyIsDown(UP_ARROW))    pos.z -= 3 * shiftMult;
+    if (keyIsDown(DOWN_ARROW))  pos.z += 3 * shiftMult;
+    if (keyIsDown(LEFT_ARROW))  pos.x -= 3 * shiftMult;
+    if (keyIsDown(RIGHT_ARROW)) pos.x += 3 * shiftMult;
+    if (keyIsDown(32)) { // Space
+      if (!jumpStartTimeMs[selected_player]) {
+        jumpStartTimeMs[selected_player] = millis();
       }
     }
   }
@@ -62,3 +80,6 @@ function getJumpHeight(i) {
   }
   return 0;
 }
+
+// Constrained map
+const cMap = (n, l1, h1, l2, h2) => map(constrain(n, l1, h1), l1, h1, l2, h2);
